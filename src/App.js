517204ee2298a608255033a595f791586c5e3759
Tomeu <tomeu.mayol@etechyachting.com>
2023-05-02 10:33:34 +0200

    ADD font family

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto px-4 font-sans">
+      <h1 className='text-3xl font-bold text-center font-abnes font-semibold'>
+        Safety Craft
+      </h1>
     </div>
   );
 }
 
-export default App;
+
